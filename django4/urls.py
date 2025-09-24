8d276ec86a2211ad72cf2a6aabc548db9054a9d8
Ozodbek <ozodbekusmonqulov7777@gmail.com>
2025-09-24 20:26:59 +0500

    f
    f

--- django4/urls.py
+++ django4/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('', TemplateView.as_view(template_name='landing_page.html'), name='landing'), 
+    path('header/', TemplateView.as_view(), name='header'),
     path("books/",include("book.urls")),
 ]
--- django4/views.py
+++ django4/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def landing_page(request):
 
     return render(request, 'landing_page.html')
+
+
+
+def header(request):
+     return render(request, 'header.html')
