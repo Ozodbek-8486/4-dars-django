1b0557ac9a51614d7d55c2354dab5afc35779b63
Ozodbek <ozodbekusmonqulov7777@gmail.com>
2025-09-30 16:11:03 +0500

    f
    f

--- users/models.py
+++ users/models.py
@@ -1,33 +1,17 @@
 from django.db import models
-from django.contrib.auth.hashers import make_password, check_password 
-from django.db import models
-from django.contrib.auth.models import User
-
-
-
-
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
-    def __str__(self):
-        return self.username
-    
+from django.conf import settings   
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,  
+        on_delete=models.CASCADE,
+        related_name="profile"    
+    )
     bio = models.TextField(blank=True, null=True)
-    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True) # rasm uchun
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)  
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
--- users/views.py
+++ users/views.py
@@ -61,25 +61,43 @@ class Logoutview(LoginRequiredMixin,View):
 
 
 
-@ login_required
+# @ login_required
+# def profile_update(request):
+#     if request.method == 'POST':
+#         user_form = UserUpdateForm(request.POST, isinstance=request.user)
+#         profile_form = ProfileUpdateForm(request.POST, request.FILES, isinstance=request.user.profile)
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+#             return redirect('users:profile')
+#     else:
+#         user_form = UserUpdateForm(instance=request.user)
+#         profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+#     context = {
+#         'user_form': user_form,
+#         'profile_update': profile_update
+#     }
+#     return redirect(request, 'user/profile_update.html' , context)
+
+
+
+
+@login_required
 def profile_update(request):
     if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST, isinstance=request.user)
-        profile_form = ProfileUpdateForm(request.POST, request.FILES, isinstance=request.user.profile)
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            return redirect('user:profile')
+            return redirect('users:profile')  # endi to'g'ri URL name ishlatiladi
     else:
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'user_form': user_form,
-        'profile_update': profile_update
+        'profile_form': profile_form
     }
-    return redirect(request, 'user/profile_update.html' , context)
-
-
-
-
+    return render(request, 'users/profile_update.html', context)
